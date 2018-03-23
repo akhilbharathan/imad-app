6721d83b902114d96230c410efb0a55bb022abe7
Akhil Bharathan <akhilbharathan@protonmail.com>
2018-03-23 13:05:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articel-one', function (req, res) {
+  res.send("Article on request will be served here");
+});
+
+app.get('/artice-two', function (req, res) {
+  res.send("Article on request will be served here");
+});
+
+app.get('/articel-three', function (req, res) {
+  res.send("Article on request will be served here");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
